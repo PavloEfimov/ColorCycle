1a4c6bb2d95ce66fc9519ca2fc01a2726db9b65b
Pavlo Yefimov <pavlo.yefimov@gmail.com>
2022-02-17 23:49:23 +0200

    windoews commit1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let inputColorBlue = document.querySelector('#inputColorBlue');
 let inputColorGreen = document.querySelector('#inputColorGreen');
 let colorDiv = document.querySelector('.colorDiv');
 let fillBtn = document.querySelector('#fillBtn');
+let inpIncrement = document.querySelector('#inpIncrement');
+let spanIncrement = document.querySelector('#spanIncrement');
 
 
 
@@ -11,12 +13,16 @@ let fillBtn = document.querySelector('#fillBtn');
 
 
 fillBtn.addEventListener('click', colorFn);
+inpIncrement.addEventListener('keydown', inpIncrFn);
 
 
 function colorFn(e){
-
         console.log(e.target.value);
         colorDiv.style.backgroundColor = `#${inputColorRed.value+inputColorBlue.value+inputColorGreen.value}`
-  
-   
+ 
+}
+function inpIncrFn(e){
+        if (e.code==='Enter'){
+                spanIncrement.textContent = `increment value: ${inpIncrement.value}`   
+        }
 }
